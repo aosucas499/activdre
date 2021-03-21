4fcc92e735478209fcd908a720f17e5e5a643650
aosucas499 <aosucas499@gmail.com>
2021-03-22 00:41:56 +0100

    add package necessary in new focal versions

--- educaandos.sh
+++ educaandos.sh
@@ -20,6 +20,7 @@ KERNELVER=$(uname -r)
 	sudo dpkg -i libssl1.0.0_1.0.2n-1ubuntu5.4_amd64.deb
 	#wget http://security.ubuntu.com/ubuntu/pool/main/i/icu/libicu60_60.2-3ubuntu3.1_amd64.deb
 	sudo dpkg -i libicu60_60.2-3ubuntu3.1_amd64.deb
+	#sudo dpkg -i zlib1g_1.2.11.dfsg-2ubuntu1_amd64.deb
 	
 	# Paquetes necesarios activinspire
 	sudo apt-get update -y
